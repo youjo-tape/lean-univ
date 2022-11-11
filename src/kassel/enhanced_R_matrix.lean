685a56e57097f8f5c028dfd6aad9672805abc308
youjo_tape <tamura.y.as@m.titech.ac.jp>
2022-11-11 16:52:41 +0900

    meeting 11/11

--- src/kassel/enhanced_R_matrix.lean
+++ src/kassel/enhanced_R_matrix.lean
@@ -26,14 +26,6 @@ def trace_2 {V: C} (f: V ⊗ V ⟶ V ⊗ V)
   ≫ (f ⊗ 𝟙 Vᘁ)    ≫ (α_ _ _ _).hom
   ≫ (𝟙 V ⊗ ε_⁻ _) ≫ (ρ_ _).hom
 
-def partial_transpose_1 {V₁ V₂ W₁ W₂: C} (f: V₁ ⊗ V₂ ⟶ W₁ ⊗ W₂)
-  :=                            (𝟙 W₁ᘁ ⊗ (ρ_ _).inv)
-  ≫ (𝟙 W₁ᘁ ⊗ 𝟙 V₂ ⊗ η_⁺ _)   ≫ (𝟙 W₁ᘁ ⊗ (α_ _ _ _).inv)
-  ≫ (𝟙 W₁ᘁ ⊗ τ_ _ _ ⊗ 𝟙 V₁ᘁ) 
-  ≫ (𝟙 W₁ᘁ ⊗ f ⊗ 𝟙 V₁ᘁ)      ≫ (𝟙 W₁ᘁ ⊗ (α_ _ _ _).hom) ≫ (α_ _ _ _).inv
-  ≫ (ε_⁺ _ ⊗ 𝟙 W₂ ⊗ 𝟙 V₁ᘁ)   ≫ (λ_ _).hom
-  ≫ τ_ _ _
-
 variable (C)
 
 structure enhanced_R_matrix (V: C) :=
@@ -51,21 +43,12 @@ structure enhanced_R_matrix (V: C) :=
   (relation_2: c.hom ≫ (μ.hom ⊗ μ.hom) = (μ.hom ⊗ μ.hom) ≫ c.hom)
   (relation_3_1: trace_2 (c.hom ≫ (𝟙 V ⊗ μ.hom)) = 𝟙 V)
   (relation_3_2: trace_2 (c.inv ≫ (𝟙 V ⊗ μ.hom)) = 𝟙 V)
-  (relation_4_1: partial_transpose_1 (τ_ _ _ ≫ c.inv) ≫ (𝟙 Vᘁ ⊗ μ.hom) ≫ partial_transpose_1 (c.hom ≫ τ_ _ _) ≫ (𝟙 Vᘁ ⊗ μ.inv) = 𝟙 (Vᘁ ⊗ V))
-  (relation_4_2: partial_transpose_1 (τ_ _ _ ≫ c.hom) ≫ (𝟙 Vᘁ ⊗ μ.hom) ≫ partial_transpose_1 (c.inv ≫ τ_ _ _) ≫ (𝟙 Vᘁ ⊗ μ.inv) = 𝟙 (Vᘁ ⊗ V))
+  (relation_4_1: (λ_ (V ⊗ Vᘁ)).inv ≫ (η_⁻ V ≫ (𝟙 Vᘁ ⊗ μ.inv) ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.inv ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ (μ.hom ⊗ 𝟙 Vᘁ) ≫ ε_⁻ V) ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ η_⁺ V) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ (ε_⁺ V ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (λ_ (V ⊗ Vᘁ)).hom = 𝟙 V ⊗ 𝟙 Vᘁ)
+  (relation_4_2: (λ_ (V ⊗ Vᘁ)).inv ≫ (η_⁻ V ≫ (𝟙 Vᘁ ⊗ μ.inv) ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ (μ.hom ⊗ 𝟙 Vᘁ) ≫ ε_⁻ V) ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ η_⁺ V) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.inv ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ (ε_⁺ V ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (λ_ (V ⊗ Vᘁ)).hom = 𝟙 V ⊗ 𝟙 Vᘁ)
+  (relation_4_3: (ρ_ (Vᘁ ⊗ V)).inv ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ η_⁺ V) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ (ε_⁺ V ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (η_⁻ V ≫ (𝟙 Vᘁ ⊗ μ.inv) ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.inv ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ (μ.hom ⊗ 𝟙 Vᘁ) ≫ ε_⁻ V) ≫ (ρ_ (Vᘁ ⊗ V)).hom = 𝟙 Vᘁ ⊗ 𝟙 V)
+  (relation_4_4: (ρ_ (Vᘁ ⊗ V)).inv ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ η_⁺ V) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.inv ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ (ε_⁺ V ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (η_⁻ V ≫ (𝟙 Vᘁ ⊗ μ.inv) ⊗ 𝟙 V ⊗ 𝟙 Vᘁ) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).hom ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).inv) ≫ (𝟙 Vᘁ ⊗ c.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 Vᘁ ⊗ (α_ V V Vᘁ).hom) ≫ (α_ Vᘁ V (V ⊗ Vᘁ)).inv ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ (μ.hom ⊗ 𝟙 Vᘁ) ≫ ε_⁻ V) ≫ (ρ_ (Vᘁ ⊗ V)).hom = 𝟙 Vᘁ ⊗ 𝟙 V)
 
 variable {C}
-
-/-
-variables (V: C) (F: Tangle ⥤ C)
-
-example: F.map ⟦β ↓ ↓⟧ ≫ F.map ⟦β⁻¹ ↓ ↓⟧ = 𝟙 _ := begin
-  rw← F.map_comp',
-  have h: (⟦β ↓ ↓⟧ ≫ ⟦β⁻¹ ↓ ↓⟧: ↓ ⊗ᵗ ↓ ⟶ ↓ ⊗ᵗ ↓) = ⟦β ↓ ↓ ≫ᵐ β⁻¹ ↓ ↓⟧,
-  apply quotient.sound, exact Tangle.hom_equiv.refl _,
-  rw h,
-end
--/
 
 namespace enhanced_R_matrix
 
@@ -76,25 +59,6 @@ variables (V: C) (R: enhanced_R_matrix C V)
   | ↓ := V
   | ↑ := Vᘁ
   | (a ⊗ᵗ b) := functor_obj a ⊗ functor_obj b
-
-/-
-@[simp] lemma id_functor_obj_id: 𝟙 (functor_obj V Tangle.id) = 𝟙 (𝟙_ C) := by refl
-@[simp] lemma id_functor_obj_hom: 𝟙 (functor_obj V ↓) = 𝟙 V := by refl
-@[simp] lemma id_functor_obj_inv: 𝟙 (functor_obj V ↑) = 𝟙 Vᘁ := by refl
-@[simp] lemma id_functor_obj_tensor {a b}: 𝟙 (functor_obj V (a ⊗ᵗ b)) = 𝟙 (functor_obj V a ⊗ functor_obj V b) := by refl
-@[simp] lemma right_unitor_functor_obj_hom: ρ_ (functor_obj V ↓) = ρ_ V := by refl
-@[simp] lemma right_unitor_functor_obj_inv: ρ_ (functor_obj V ↑) = ρ_ Vᘁ := by refl
-@[simp] lemma right_unitor_functor_obj_tensor {a b}: ρ_ (functor_obj V (a ⊗ᵗ b)) = ρ_ (functor_obj V a ⊗ functor_obj V b) := by refl
-@[simp] lemma left_unitor_functor_obj_hom: λ_ (functor_obj V ↓) = λ_ V := by refl
-@[simp] lemma left_unitor_functor_obj_inv: λ_ (functor_obj V ↑) = λ_ Vᘁ := by refl
-@[simp] lemma left_unitor_functor_obj_tensor {a b}: λ_ (functor_obj V (a ⊗ᵗ b)) = λ_ (functor_obj V a ⊗ functor_obj V b) := by refl
-@[simp] lemma associator_functor_obj_hom_1 {a b}: α_ (functor_obj V ↓) a b = α_ V a b := by refl
-@[simp] lemma associator_functor_obj_hom_2 {a b}: α_ a (functor_obj V ↓) b = α_ a V b := by refl
-@[simp] lemma associator_functor_obj_hom_3 {a b}: α_ a b (functor_obj V ↓) = α_ a b V := by refl
-@[simp] lemma associator_functor_obj_inv_1 {a b}: α_ (functor_obj V ↑) a b = α_ Vᘁ a b := by refl
-@[simp] lemma associator_functor_obj_inv_2 {a b}: α_ a (functor_obj V ↑) b = α_ a Vᘁ b := by refl
-@[simp] lemma associator_functor_obj_inv_3 {a b}: α_ a b (functor_obj V ↑) = α_ a b Vᘁ := by refl
--/
 
 def functor_map: Π {X Y}, (X ⟶ᵐ Y) → (functor_obj V X ⟶ functor_obj V Y)
   | _ _ (𝟙ᵐ a) := 𝟙 (functor_obj V a)
@@ -116,15 +80,15 @@ def functor_map: Π {X Y}, (X ⟶ᵐ Y) → (functor_obj V X ⟶ functor_obj V Y
 
 namespace aux
 
-lemma relation_2_c_inv: R.c.inv ≫ (R.μ.hom ⊗ R.μ.hom) = (R.μ.hom ⊗ R.μ.hom) ≫ R.c.inv :=
+lemma relation_2_c_inv:
+  R.c.inv ≫ (R.μ.hom ⊗ R.μ.hom) = (R.μ.hom ⊗ R.μ.hom) ≫ R.c.inv :=
 by rw [iso.eq_comp_inv, category.assoc, iso.inv_comp_eq, R.relation_2]
 
 lemma functor_map_well_defined_1_1:
   functor_map V R (ρ⁻¹ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ η⁻ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ ε⁻ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ ℓ _) =
   functor_map V R (𝟙ᵐ _) :=
 begin
-  simp_rw functor_map,
-  change (ρ_ _).inv ≫ (𝟙 _ ⊗ η_⁻ V ≫ (𝟙 _ ⊗ R.μ.inv)) ≫ (α_ _ _ _).inv ≫ ((R.μ.hom ⊗ 𝟙 _) ≫ ε_⁻ V ⊗ 𝟙 _) ≫ (λ_ _).hom = 𝟙 _,
+  dsimp [functor_map],
   simp_rw [id_tensor_comp, comp_tensor_id, category.assoc],
   
   rw ←associator_inv_naturality_assoc,
@@ -141,26 +105,25 @@ begin
   simp_rw [category.assoc, iso.inv_hom_id_assoc],
   rw iso.hom_inv_id,
 end
+
 lemma functor_map_well_defined_1_2:
   functor_map V R (ℓ⁻¹ _ ≫ᵐ η⁺ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ ε⁺ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ _) :=
 begin
-  simp_rw functor_map,
-  change (λ_ _).inv ≫ (η_⁺ V ⊗ 𝟙 _) ≫ (α_ _ _ _).hom ≫ (𝟙 _ ⊗ ε_⁺ V) ≫ (ρ_ _).hom = 𝟙 _,
+  dsimp [functor_map],
   rw [evaluation_coevaluation_assoc, iso.inv_hom_id_assoc, iso.inv_hom_id],
 end
 
 lemma functor_map_well_defined_2_1:
   functor_map V R (ρ⁻¹ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ η⁺ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ ε⁺ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ ℓ _) = functor_map V R (𝟙ᵐ _) :=
 begin
-  simp_rw functor_map,
-  change (ρ_ _).inv ≫ (𝟙 _ ⊗ η_⁺ V) ≫ (α_ _ _ _).inv ≫ (ε_⁺ V ⊗ 𝟙 _) ≫ (λ_ _).hom = 𝟙 _,
+  dsimp [functor_map],
   rw [coevaluation_evaluation_assoc, iso.inv_hom_id_assoc, iso.inv_hom_id],
 end
+
 lemma functor_map_well_defined_2_2:
   functor_map V R (ℓ⁻¹ _ ≫ᵐ η⁻ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ ε⁻ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ _) :=
 begin
-  simp_rw functor_map,
-  change (λ_ _).inv ≫ (η_⁻ V ≫ (𝟙 _ ⊗ R.μ.inv) ⊗ 𝟙 _) ≫ (α_ _ _ _).hom ≫ (𝟙 _ ⊗ (R.μ.hom ⊗ 𝟙 _) ≫ ε_⁻ V) ≫ (ρ_ _).hom = 𝟙 _,
+  dsimp [functor_map],
   simp_rw [id_tensor_comp, comp_tensor_id, category.assoc],
   rw associator_naturality_assoc,
   slice_lhs 4 5 { rw [←tensor_comp, ←tensor_comp, category.comp_id, iso.inv_hom_id, tensor_id, tensor_id], },
@@ -176,6 +139,7 @@ abbreviation functor_map_well_defined_3_lhs (b: ↓ ⊗ᵗ ↓ ⟶ᵐ ↓ ⊗ᵗ
     ≫ᵐ (𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑) ⊗ᵐ 𝟙ᵐ ↓ ⊗ᵐ ε⁻ ⊗ᵐ 𝟙ᵐ ↑ ≫ᵐ (𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑) ⊗ᵐ 𝟙ᵐ ↓ ⊗ᵐ ℓ _
     ≫ᵐ (𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑) ⊗ᵐ ε⁻                 ≫ᵐ ρ _
   )
+
 abbreviation functor_map_well_defined_3_rhs (b: ↓ ⊗ᵗ ↓ ⟶ᵐ ↓ ⊗ᵗ ↓) :=
   functor_map V R (                             ρ⁻¹ _
     ≫ᵐ (𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑) ⊗ᵐ η⁺                 ≫ᵐ (𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑) ⊗ᵐ 𝟙ᵐ ↓ ⊗ᵐ ℓ⁻¹ ↑
@@ -184,6 +148,7 @@ abbreviation functor_map_well_defined_3_rhs (b: ↓ ⊗ᵗ ↓ ⟶ᵐ ↓ ⊗ᵗ
     ≫ᵐ (𝟙ᵐ ↑ ⊗ᵐ ε⁺ ⊗ᵐ 𝟙ᵐ ↓) ⊗ᵐ 𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑ ≫ᵐ (𝟙ᵐ ↑ ⊗ᵐ ℓ _) ⊗ᵐ 𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑
     ≫ᵐ ε⁺                   ⊗ᵐ 𝟙ᵐ ↑ ⊗ᵐ 𝟙ᵐ ↑ ≫ᵐ ℓ _
   )
+
 abbreviation functor_map_well_defined_3_mid (b: ↓ ⊗ᵗ ↓ ⟶ᵐ ↓ ⊗ᵗ ↓) :=
   (δ_ V V).inv ≫ (functor_map V R b)ᘁ ≫ (δ_ V V).hom
 
@@ -193,8 +158,7 @@ lemma functor_map_well_defined_3_left (b: ↓ ⊗ᵗ ↓ ⟶ᵐ ↓ ⊗ᵗ ↓) 
 begin
   dunfold functor_map_well_defined_3_lhs,
   dunfold functor_map_well_defined_3_mid,
-  simp_rw functor_map,
-  change (λ_ _).inv ≫ (η_⁻ V ≫ (𝟙 _ ⊗ R.μ.inv) ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((𝟙 _ ⊗ (λ_ _).inv) ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((𝟙 _ ⊗ η_⁻ V ≫ (𝟙 _ ⊗ R.μ.inv) ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((𝟙 _ ⊗ (α_ _ _ _).hom) ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((α_ _ _ _).inv ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ (α_ _ _ _).hom ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ functor_map V R b ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ (α_ V V _).hom) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ (α_ _ _ _).inv) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ (R.μ.hom ⊗ 𝟙 _) ≫ ε_⁻ V ⊗ 𝟙 _) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ (λ_ _).hom) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ (R.μ.hom ⊗ 𝟙 _) ≫ ε_⁻ V) ≫ (ρ_ _).hom = (δ_ _ _).inv ≫ (functor_map V R b)ᘁ ≫ (δ_ V V).hom,
+  dsimp [functor_map],
   simp only [tensor_id, id_tensor_comp, comp_tensor_id, category.assoc],
   
   iterate 6 { rw [←tensor_comp_assoc _ (𝟙 (Vᘁ ⊗ Vᘁ)) _ (𝟙 (Vᘁ ⊗ Vᘁ)), category.comp_id], repeat { rw category.assoc, }, },
@@ -229,14 +193,14 @@ begin
   simp_rw category.assoc, rw right_adjoint_mate_rev,
   rw [h, ←tensor_iso_hom, ←tensor_iso_inv, iso.inv_hom_id_assoc],
 end
+
 lemma functor_map_well_defined_3_right (b: ↓ ⊗ᵗ ↓ ⟶ᵐ ↓ ⊗ᵗ ↓):
   functor_map_well_defined_3_rhs V R b =
   functor_map_well_defined_3_mid V R b :=
 begin
   dunfold functor_map_well_defined_3_rhs,
   dunfold functor_map_well_defined_3_mid,
-  simp_rw functor_map,
-  change (ρ_ _).inv ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ η_⁺ V) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ (λ_ _).inv) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ η_⁺ V ⊗ 𝟙 _) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ (α_ _ _ _).hom) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ (α_ _ _ _).inv) ≫ ((𝟙 Vᘁ ⊗ 𝟙 _) ⊗ functor_map V R b ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ (α_ _ (V ⊗ V) _).inv ≫ ((α_ _ _ _).hom ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((𝟙 _ ⊗ (α_ _ _ _).inv) ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((𝟙 _ ⊗ ε_⁺ V ⊗ 𝟙 _) ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ ((𝟙 _ ⊗ (λ_ _).hom) ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ (ε_⁺ V ⊗ 𝟙 _ ⊗ 𝟙 Vᘁ) ≫ (λ_ _).hom = (δ_ V V).inv ≫ (functor_map V R b)ᘁ ≫ (δ_ V V).hom,
+  dsimp [functor_map],
   simp only [tensor_id, id_tensor_comp, comp_tensor_id, category.assoc],
   
   iterate 4 { rw ←tensor_comp_assoc (𝟙 (Vᘁ ⊗ Vᘁ)) _ (𝟙 (Vᘁ ⊗ Vᘁ)) _, rw category.comp_id, }, repeat { rw category.assoc, },
@@ -254,7 +218,7 @@ begin
   rw [←tensor_id_comp_id_tensor_assoc _ (δ_ _ _).inv, ←right_unitor_inv_naturality_assoc],
   rw iso.cancel_iso_inv_left,
   
-  slice_lhs 3 5 { simp only [←tensor_comp, category.comp_id], rw @category.comp_id _ _ (V ⊗ V) (V ⊗ V) (functor_map V R b), }, simp_rw category.assoc,
+  slice_lhs 3 5 { simp only [←tensor_comp, category.comp_id], rw @category.id_comp _ _ (V ⊗ V) (V ⊗ V) (functor_map V R b), }, simp_rw category.assoc,
   rw associator_inv_naturality_assoc,
   rw ←tensor_id_comp_id_tensor_assoc (δ_ V V).hom _,
   rw [←tensor_comp_assoc _ (δ_ _ _).hom _ _, ←id_comp_comp_id, tensor_comp_assoc],
@@ -264,12 +228,14 @@ begin
   simp_rw [tensor_id, category.id_comp],
   rw [←associator_inv_naturality_assoc], rw right_adjoint_mate,
 end
+
 lemma functor_map_well_defined_3_1:
   functor_map_well_defined_3_lhs V R β =
   functor_map_well_defined_3_rhs V R β :=
   eq.trans
     (functor_map_well_defined_3_left V R β (by rw [functor_map, R.relation_2]))
     (functor_map_well_defined_3_right V R β).symm
+
 lemma functor_map_well_defined_3_2:
   functor_map_well_defined_3_lhs V R β⁻¹ =
   functor_map_well_defined_3_rhs V R β⁻¹ :=
@@ -277,57 +243,54 @@ lemma functor_map_well_defined_3_2:
     (functor_map_well_defined_3_left V R β⁻¹ (by rw [functor_map, relation_2_c_inv]))
     (functor_map_well_defined_3_right V R β⁻¹).symm
 
-lemma functor_map_well_defined_4_1: functor_map V R (β ≫ᵐ β⁻¹) = functor_map V R (𝟙ᵐ (↓ ⊗ᵗ ↓)) := by simp [functor_map]
-lemma functor_map_well_defined_4_2: functor_map V R (β⁻¹ ≫ᵐ β) = functor_map V R (𝟙ᵐ (↓ ⊗ᵗ ↓)) := by simp [functor_map]
+lemma functor_map_well_defined_4_1:
+  functor_map V R (β ≫ᵐ β⁻¹) = functor_map V R (𝟙ᵐ (↓ ⊗ᵗ ↓)) :=
+by simp [functor_map]
+
+lemma functor_map_well_defined_4_2:
+  functor_map V R (β⁻¹ ≫ᵐ β) = functor_map V R (𝟙ᵐ (↓ ⊗ᵗ ↓)) :=
+by simp [functor_map]
 
 lemma functor_map_well_defined_5:
-functor_map V R (α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ ↓ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ β ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ _) = functor_map V R (𝟙ᵐ ↓ ⊗ᵐ β ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ ↓ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ β ≫ᵐ α⁻¹ _ _ _) := begin
-  simp [functor_map],
-  change (α_ _ _ _).inv ≫ (R.c.hom ⊗ 𝟙 V) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.c.hom) ≫ (α_ _ _ _).inv ≫ (R.c.hom ⊗ 𝟙 V) = (𝟙 V ⊗ R.c.hom) ≫ (α_ _ _ _).inv ≫ (R.c.hom ⊗ 𝟙 V) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.c.hom) ≫ (α_ _ _ _).inv,
-  exact R.relation_1.symm,
-end
+  functor_map V R (α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ ↓ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ β ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ _) = functor_map V R (𝟙ᵐ ↓ ⊗ᵐ β ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ ↓ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ β ≫ᵐ α⁻¹ _ _ _) :=
+by dsimp [functor_map]; exact R.relation_1.symm
 
 lemma functor_map_well_defined_6_1:
-functor_map V R (ρ⁻¹ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ η⁺ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ ↑ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ ε⁻ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ ↓) := begin
+  functor_map V R (ρ⁻¹ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ η⁺ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β ⊗ᵐ 𝟙ᵐ ↑ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ ε⁻ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ ↓) :=
+begin
   simp [functor_map],
   change (ρ_ _).inv ≫ (𝟙 V ⊗ η_⁺ V) ≫ (α_ _ _ _).inv ≫ (R.c.hom ⊗ 𝟙 Vᘁ) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.μ.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 V ⊗ ε_⁻ V) ≫ (ρ_ _).hom = 𝟙 V,
-  have h: trace_2 (R.c.hom ≫ (𝟙 V ⊗ R.μ.hom)) = (ρ_ _).inv ≫ (𝟙 V ⊗ η_⁺ V) ≫ (α_ _ _ _).inv ≫ (R.c.hom ⊗ 𝟙 Vᘁ) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.μ.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 V ⊗ ε_⁻ V) ≫ (ρ_ _).hom := by simp [functor_map, trace_2, coevaluation, evaluation, evaluation_rev],
+  have h: trace_2 (R.c.hom ≫ (𝟙 V ⊗ R.μ.hom)) = (ρ_ _).inv ≫ (𝟙 V ⊗ η_⁺ V) ≫ (α_ _ _ _).inv ≫ (R.c.hom ⊗ 𝟙 Vᘁ) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.μ.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 V ⊗ ε_⁻ V) ≫ (ρ_ _).hom :=
+    by simp [functor_map, trace_2, coevaluation, evaluation, evaluation_rev],
   rw ←h,
   exact R.relation_3_1,
 end
 lemma functor_map_well_defined_6_2:
-functor_map V R (ρ⁻¹ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ η⁺ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β⁻¹ ⊗ᵐ 𝟙ᵐ ↑ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ ε⁻ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ ↓) := begin
+  functor_map V R (ρ⁻¹ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ η⁺ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ β⁻¹ ⊗ᵐ 𝟙ᵐ ↑ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↓ ⊗ᵐ ε⁻ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ ↓) :=
+begin
   simp [functor_map],
   change (ρ_ _).inv ≫ (𝟙 V ⊗ η_⁺ V) ≫ (α_ _ _ _).inv ≫ (R.c.inv ⊗ 𝟙 Vᘁ) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.μ.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 V ⊗ ε_⁻ V) ≫ (ρ_ _).hom = 𝟙 V,
-  have h: trace_2 (R.c.inv ≫ (𝟙 V ⊗ R.μ.hom)) = (ρ_ _).inv ≫ (𝟙 V ⊗ η_⁺ V) ≫ (α_ _ _ _).inv ≫ (R.c.inv ⊗ 𝟙 Vᘁ) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.μ.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 V ⊗ ε_⁻ V) ≫ (ρ_ _).hom := by simp [functor_map, trace_2, coevaluation, evaluation, evaluation_rev],
+  have h: trace_2 (R.c.inv ≫ (𝟙 V ⊗ R.μ.hom)) = (ρ_ _).inv ≫ (𝟙 V ⊗ η_⁺ V) ≫ (α_ _ _ _).inv ≫ (R.c.inv ⊗ 𝟙 Vᘁ) ≫ (α_ _ _ _).hom ≫ (𝟙 V ⊗ R.μ.hom ⊗ 𝟙 Vᘁ) ≫ (𝟙 V ⊗ ε_⁻ V) ≫ (ρ_ _).hom :=
+    by simp [functor_map, trace_2, coevaluation, evaluation, evaluation_rev],
   rw ←h,
   exact R.relation_3_2,
 end
 
--- lemma functor_map_well_defined_8_2:
-/-
 lemma functor_map_well_defined_7_1:
-functor_map V R (ℓ⁻¹ _ ⊗ᵐ 𝟙 ↑ ≫ᵐ η⁻ ⊗ᵐ 𝟙 ↓ ⊗ᵐ 𝟙 ↑ ≫ᵐ α _ _ _ ⊗ᵐ 𝟙 ↑ ≫ᵐ 𝟙 ↑ ⊗ᵐ β⁻¹ ⊗ᵐ 𝟙 ↑ ≫ᵐ α⁻¹ _ _ _ ⊗ᵐ 𝟙 ↑ ≫ᵐ α _ _ _ ≫ᵐ 𝟙 ↑ ⊗ᵐ 𝟙 ↓ ⊗ᵐ ε⁻ ≫ᵐ 𝟙 ↑ ⊗ᵐ 𝟙 ↓ ⊗ᵐ η⁺ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ α _ _ _ ⊗ᵐ 𝟙 ↑ ≫ᵐ 𝟙 ↑ ⊗ᵐ β ⊗ᵐ 𝟙 ↑ ≫ᵐ α⁻¹ _ _ _ ⊗ᵐ 𝟙 ↑ ≫ᵐ ε⁺ ⊗ᵐ 𝟙 ↓ ⊗ᵐ 𝟙 ↑ ≫ᵐ ℓ _ ⊗ᵐ 𝟙 ↑) = functor_map V R (𝟙 ↓ ⊗ᵐ 𝟙 ↑) := begin
-  simp only [functor_map, category.assoc, tensor_comp_id, comp_tensor_id],
-  change ((λ_ _).inv ⊗ 𝟙 Vᘁ)
-  ≫ ((η_⁻ V ⊗ 𝟙 V) ⊗ 𝟙 Vᘁ)
-  ≫ (((𝟙 Vᘁ ⊗ R.μ.inv) ⊗ 𝟙 V) ⊗ 𝟙 Vᘁ)
-  ≫ ((α_ _ _ _).hom ⊗ 𝟙 Vᘁ)
-  ≫ ((𝟙 Vᘁ ⊗ R.c.inv) ⊗ 𝟙 Vᘁ)
-  ≫ ((α_ _ _ _).inv ⊗ 𝟙 Vᘁ)
-  ≫ (α_ _ _ _).hom
-  ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ R.μ.hom ⊗ 𝟙 Vᘁ)
-  ≫ (𝟙 (Vᘁ ⊗ V) ⊗ ε_⁻ V)
-  ≫ ((𝟙 Vᘁ ⊗ 𝟙 V) ⊗ η_⁺ V)
-  ≫ (α_ _ _ _).inv
-  ≫ ((α_ _ _ _).hom ⊗ 𝟙 Vᘁ)
-  ≫ ((𝟙 Vᘁ ⊗ R.c.hom) ⊗ 𝟙 Vᘁ)
-  ≫ ((α_ _ _ _).inv ⊗ 𝟙 Vᘁ)
-  ≫ ((ε_⁺ V ⊗ 𝟙 V) ⊗ 𝟙 Vᘁ)
-  ≫ ((λ_ _).hom ⊗ 𝟙 Vᘁ)
-  = 𝟙 V ⊗ 𝟙 Vᘁ,
-end
--/
+  functor_map V R (ℓ⁻¹ _ ≫ᵐ η⁻ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β⁻¹ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ ε⁻ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ η⁺ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ ε⁺ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ ℓ _) = functor_map V R (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) :=
+by dsimp [functor_map]; exact R.relation_4_1
+
+lemma functor_map_well_defined_7_2:
+  functor_map V R (ℓ⁻¹ _ ≫ᵐ η⁻ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ ε⁻ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ η⁺ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β⁻¹ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ ε⁺ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ ℓ _) = functor_map V R (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) :=
+by dsimp [functor_map]; exact R.relation_4_2
+
+lemma functor_map_well_defined_8_1:
+  functor_map V R (ρ⁻¹ _ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ η⁺ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↑ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ ε⁺ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ η⁻ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β⁻¹ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ ↑ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ ε⁻ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) :=
+by dsimp [functor_map]; exact R.relation_4_3
+
+lemma functor_map_well_defined_8_2:
+  functor_map V R (ρ⁻¹ _ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ η⁺ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ ↑ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β⁻¹ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ ε⁺ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ η⁻ ⊗ᵐ 𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _ ≫ᵐ α _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ α⁻¹ _ _ _ ≫ᵐ 𝟙ᵐ _ ⊗ᵐ β ⊗ᵐ 𝟙ᵐ _ ≫ᵐ 𝟙ᵐ ↑ ⊗ᵐ α _ _ _ ≫ᵐ α⁻¹ _ _ _ ≫ᵐ (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) ⊗ᵐ ε⁻ ≫ᵐ ρ _) = functor_map V R (𝟙ᵐ _ ⊗ᵐ 𝟙ᵐ _) :=
+by dsimp [functor_map]; exact R.relation_4_4
 
 lemma functor_map_well_defined {X Y}: ∀ (f g: X ⟶ᵐ Y), f ≈ g → functor_map V R f = functor_map V R g := begin
   intros f g r, induction r,
@@ -352,21 +315,21 @@ lemma functor_map_well_defined {X Y}: ∀ (f g: X ⟶ᵐ Y), f ≈ g → functor
   { simp only [functor_map, monoidal_category.right_unitor_naturality'], dsimp at *, simp at *, },
   { dsimp [functor_map], rw monoidal_category.pentagon', },
   { simp only [functor_map, monoidal_category.triangle'], dsimp at *, simp at *, },
-  exact aux.functor_map_well_defined_1_1 _ _,
-  exact aux.functor_map_well_defined_1_2 _ _,
-  exact aux.functor_map_well_defined_2_1 _ _,
-  exact aux.functor_map_well_defined_2_2 _ _,
-  exact aux.functor_map_well_defined_3_1 _ _,
-  exact aux.functor_map_well_defined_3_2 _ _,
-  exact aux.functor_map_well_defined_4_1 _ _,
-  exact aux.functor_map_well_defined_4_2 _ _,
-  exact aux.functor_map_well_defined_5 _ _,
-  exact aux.functor_map_well_defined_6_1 _ _,
-  exact aux.functor_map_well_defined_6_2 _ _,
-  sorry,
-  sorry,
-  sorry,
-  sorry,
+  exact aux.functor_map_well_defined_1_1 V R,
+  exact aux.functor_map_well_defined_1_2 V R,
+  exact aux.functor_map_well_defined_2_1 V R,
+  exact aux.functor_map_well_defined_2_2 V R,
+  exact aux.functor_map_well_defined_3_1 V R,
+  exact aux.functor_map_well_defined_3_2 V R,
+  exact aux.functor_map_well_defined_4_1 V R,
+  exact aux.functor_map_well_defined_4_2 V R,
+  exact aux.functor_map_well_defined_5 V R,
+  exact aux.functor_map_well_defined_6_1 V R,
+  exact aux.functor_map_well_defined_6_2 V R,
+  exact aux.functor_map_well_defined_7_1 V R,
+  exact aux.functor_map_well_defined_7_2 V R,
+  exact aux.functor_map_well_defined_8_1 V R,
+  exact aux.functor_map_well_defined_8_2 V R,
 end
 
 end aux
